9e566735b73a748195ad35670ddae54cf88840f1
karoliskersys <kersys.karolis@inbox.lt>
2023-05-29 12:52:11 +0300

    Login component creation

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,29 +1,17 @@
 import React from 'react';
-import Welcome, {UserButton, WelcomeCopy} from "../components/Welcome";
+import Login from "./Login";
+
 
 
 function App() {
-const welcome =  <WelcomeCopy user={{
-  name: "PetrasCopy",
-  surname: "Petraitis"}} salary={1500}/>
 
-  const loopOf5Welcomes = () =>{
-  return Array.from(Array(5).keys()).map((val:number)=> welcome);
-  }
-
-  return (
+    return (
     <div>
-      <Welcome name={"Petras"} surname={"Petraitis"}/>
+      Uzd 1
         <br/>
-        <Welcome name={"Antanas"}/>
-        <br/>
-        <br/>
-        <input/>
-        <UserButton/>
-        <br/>
-      {welcome}
-      {loopOf5Welcomes()}
+        <Login value={'admin'}/>
     </div>
+
   );
 }
 
